bee063c8044c1d5383f87ff23287d9a188773248
krishnavamsee96 <66152428+krishnavamsee96@users.noreply.github.com>
2020-05-31 05:20:15 -0500

    Add files via upload

---
+++ rom.py
@@ -0,0 +1,64 @@
+# VENKATA BHARAT MADALA
+# 800690180
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BOARD)
+button1 = 11
+button2 = 13
+button3 = 15
+button4 = 32
+LED1 = 29
+LED2 = 31
+LED3 = 33
+GPIO.setup(button1,GPIO.IN,pull_up_down=GPIO.PUD_UP)
+GPIO.setup(button2,GPIO.IN,pull_up_down=GPIO.PUD_UP)
+GPIO.setup(button3,GPIO.IN,pull_up_down=GPIO.PUD_UP)
+GPIO.setup(LED1,GPIO.OUT)
+GPIO.setup(LED2,GPIO.OUT)
+GPIO.setup(LED3,GPIO.OUT)
+BS1 = False
+BS2 = False
+BS3 = False
+while(1):
+	if GPIO.input(button1)==0:
+		print('Button1 ')
+		if BS1==False:
+			GPIO.output(LED1,True)
+			BS1 = True
+			time.sleep(1)
+	if GPIO.input(button2)==0:
+		print('Button2 ')
+		if BS1==False:
+			GPIO.output(LED1,True)
+			BS1 = True
+			time.sleep(1)
+		if BS2==False:
+			GPIO.output(LED2,True)
+			BS2 = True
+			time.sleep(1)
+	if GPIO.input(button3)==0:
+		print('Button3 ')
+		if BS1==False:
+			GPIO.output(LED1,True)
+			BS1 = True
+			time.sleep(1)
+		if BS2==False:
+			GPIO.output(LED2,True)
+			BS2 = True
+			time.sleep(1)
+		if BS3==False:
+			GPIO.output(LED3,True)
+			BS3 = True
+			time.sleep(1)
+	if GPIO.input(button4)==0:
+		GPIO.output(LED1,False)
+			BS1 = False
+			time.sleep(1)
+		GPIO.output(LED2,False)
+			BS2 = False
+			time.sleep(1)
+		GPIO.output(LED3,False)
+			BS3 = False
+			time.sleep(1)	
+GPIO.cleanup()
+	
